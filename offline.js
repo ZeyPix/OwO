96ef2735a482607fcdf13612f117d95f1eaa94b1
zey <benblaise930@gmail.com>
2018-03-05 01:01:16 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1520207980,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/roselia-sheet0.png",
+		"images/moonbackgound-sheet0.png",
+		"images/dialogbox-sheet0.png",
+		"images/tilemap.png",
+		"images/playerrosélia-sheet0.png",
+		"images/bulletroselia-sheet0.png",
+		"images/bossdeoufça-sheet0.png",
+		"images/ennemybullet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
